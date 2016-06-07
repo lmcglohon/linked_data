deca5395ff2334a1006127bfc2f4ac851e684afe
Laney McGlohon <laneymcg@stanford.edu>
2016-06-07 14:50:00 -0700

    refactored

--- spec/lib/performance_group_spec.rb
+++ spec/lib/performance_group_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 require 'PerformanceGroup'
 
 describe 'a PerformanceGroup' do
+  before :each do
+    @foo = PerformanceGroup.new
+  end
   it 'can instantiate' do
-    foo = PerformanceGroup.new
-    expect(foo).to be_kind_of PerformanceGroup
+    expect(@foo).to be_kind_of PerformanceGroup
   end
   it 'has a name' do
-    foo = PerformanceGroup.new
-    foo.name = 'The Beatles'
-    expect(foo.name).to eq 'The Beatles'
+    @foo.name = 'The Beatles'
+    expect(@foo.name).to eq 'The Beatles'
   end
   it 'has a list of albums' do
     albums = ['Help', "A Hard Day's Night", "Sgt. Pepper's"]
-    foo = PerformanceGroup.new
-    foo.albums = albums
-    expect(foo.albums).to eq ['Help', "A Hard Day's Night", "Sgt. Pepper's"]
+    @foo.albums = albums
+    expect(@foo.albums).to eq ['Help', "A Hard Day's Night", "Sgt. Pepper's"]
   end
 end
